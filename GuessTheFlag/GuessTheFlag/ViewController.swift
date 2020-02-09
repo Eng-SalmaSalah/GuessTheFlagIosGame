02b3ddf983f1b10c65893dcf857b6448c41f679c
Salma Salah <salma.salah@fawry.com>
2020-02-09 11:14:17 +0200

    init the project

---
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Salma Salah on 2/9/20.
+//  Copyright © 2020 Salma Salah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
