0709dca737487b0845a3eb13ba99fdef011725f8
Salma Salah <salma.salah@fawry.com>
2020-02-09 21:56:03 +0200

    solving the challenge

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         initCountriesArray()
@@ -41,13 +42,20 @@ class ViewController: UIViewController {
     }
     
     func askQuestion(action : UIAlertAction! = nil){ // we added this param to call this method in action handler of the alert btn , it sets uialert as nil if not received as default value
-        
+        if counter == 10{
+            let scoreAlert = UIAlertController(title: "Score", message: "Your Final Score is \(score) out of 10", preferredStyle: .alert)
+            scoreAlert.addAction(UIAlertAction(title: "Play Again", style: .default, handler: resetGame))
+            title = "Your Score :\(score)"
+            present(scoreAlert, animated: true)
+            return
+        }
         // to show different flags each time
         countries.shuffle()
         
         //to show the name of the country to select as title
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        let countryToSelect = countries[correctAnswer].uppercased()
+        title = "Your Score :\(score) - select \(countryToSelect)'s flag"
         
         flagOneBtn.setImage(UIImage(named: countries[0]), for: .normal)
         flagTwoBtn.setImage(UIImage(named: countries[1]), for: .normal)
@@ -55,16 +63,27 @@ class ViewController: UIViewController {
 
     }
     
+    func resetGame (action : UIAlertAction!){
+        counter = 0
+        score = 0
+        askQuestion()
+    }
+    
     @IBAction func buttonPressedAction(_ sender: UIButton) {
+        counter += 1
         var title : String?
+        var alertBody : String?
         if sender.tag == correctAnswer{
             title = "Correct!"
+            alertBody = "Yes!,that is the flag of \(countries[correctAnswer].uppercased())"
             score += 1
         }else{
             title = "Wrong!"
+            alertBody = "No!,that is the flag of \(countries[sender.tag].uppercased())"
             score -= 1
         }
-        let alertController = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+//        let alertController = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        let alertController = UIAlertController(title: title, message: alertBody, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) //here we path only the name of the function to run when btn pressed , don't call the method askQuestion() bec if we do this it means call the method askQuestion now to get the name of the method to run when btn pressed
         present(alertController, animated: true)
     }
